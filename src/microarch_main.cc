71eec9e88edd709a1d4152ac9413e66311f3ab9d
guangdadong.dong <ghostyuan@outlook.com>
2024-09-22 11:35:04 +0800

    update code format

--- src/microarch_main.cc
+++ src/microarch_main.cc
@@ -11,116 +11,116 @@ using namespace std;
 
 class tensor {
   public:
-    struct micrarch_tensor_descriptor{
-      int base_addr;
-      int unit_num;
-      int slice_num;
-      int plane_num;
-      int cube_num;
-      int unit_skip;
-      int slice_skip;
-      int plane_skip;
-      int cube_skip;
+    struct micrarchTensorDescriptor{
+      int baseAddr;
+      int unitNum;
+      int sliceNum;
+      int planeNum;
+      int cubeNum;
+      int unitSkip;
+      int sliceSkip;
+      int planeSkip;
+      int cubeSkip;
     };
 
-    micrarch_tensor_descriptor tensor_desc;
-    micrarch_tensor_descriptor sub_tensor_desc;
+    micrarchTensorDescriptor tensorDesc;
+    micrarchTensorDescriptor subTensorDesc;
 
     tensor(){
-      tensor_desc.base_addr  = gen_random_data(100) * 64;
-      tensor_desc.unit_num   = gen_random_data(40);
-      tensor_desc.slice_num  = gen_random_data(10);
-      tensor_desc.plane_num  = gen_random_data(20);
-      tensor_desc.cube_num   = gen_random_data(6);
+      tensorDesc.baseAddr  = genRandomData(100) * 64;
+      tensorDesc.unitNum   = genRandomData(40);
+      tensorDesc.sliceNum  = genRandomData(10);
+      tensorDesc.planeNum  = genRandomData(20);
+      tensorDesc.cubeNum   = genRandomData(6);
 
-      tensor_desc.unit_skip  = 1<<static_cast<int>(ceil(log2(gen_random_data(64))));
-      tensor_desc.slice_skip = tensor_desc.unit_num  * tensor_desc.unit_skip;
-      tensor_desc.plane_skip = tensor_desc.slice_num * tensor_desc.slice_skip;
-      tensor_desc.cube_skip  = tensor_desc.plane_num * tensor_desc.plane_skip;
+      tensorDesc.unitSkip  = 1<<static_cast<int>(ceil(log2(genRandomData(64))));
+      tensorDesc.sliceSkip = tensorDesc.unitNum  * tensorDesc.unitSkip;
+      tensorDesc.planeSkip = tensorDesc.sliceNum * tensorDesc.sliceSkip;
+      tensorDesc.cubeSkip  = tensorDesc.planeNum * tensorDesc.planeSkip;
     }
 
     ~tensor(){
     }
 
-    unsigned int gen_random_data(unsigned int max_value=10){
+    unsigned int genRandomData(unsigned int maxValue=10){
       std::srand(static_cast<unsigned int>(std::time(0)));
-      unsigned int randomNum = std::rand() % max_value + 1;
+      unsigned int randomNum = std::rand() % maxValue + 1;
 
       return randomNum;
     }
 
-    void stringTensor(micrarch_tensor_descriptor myTensorDesc){
-      cout <<"unit_num:"   << myTensorDesc.unit_num <<", slice_num:" << myTensorDesc.slice_num <<", plane_num:" << myTensorDesc.plane_num <<", cube_num:"<< myTensorDesc.cube_num << endl;
-      cout <<"unit_skip : "<< myTensorDesc.unit_skip  << endl;
-      cout <<"slice_skip: "<< myTensorDesc.slice_skip << endl;
-      cout <<"plane_skip: "<< myTensorDesc.plane_skip << endl;
-      cout <<"cube_skip:  "<< myTensorDesc.cube_skip  << endl;
+    void stringTensor(micrarchTensorDescriptor myTensorDesc){
+      cout <<"unitNum:"   << myTensorDesc.unitNum <<", sliceNum:" << myTensorDesc.sliceNum <<", planeNum:" << myTensorDesc.planeNum <<", cubeNum:"<< myTensorDesc.cubeNum << endl;
+      cout <<"unitSkip : "<< myTensorDesc.unitSkip  << endl;
+      cout <<"sliceSkip: "<< myTensorDesc.sliceSkip << endl;
+      cout <<"planeSkip: "<< myTensorDesc.planeSkip << endl;
+      cout <<"cubeSkip:  "<< myTensorDesc.cubeSkip  << endl;
       cout <<"" << endl;
     }
 
-    void gen_sub_tensor(){
-      sub_tensor_desc.base_addr  = tensor_desc.base_addr;
-      sub_tensor_desc.unit_num   = gen_random_data(tensor_desc.unit_num);
-      sub_tensor_desc.slice_num  = gen_random_data(tensor_desc.slice_num);
-      sub_tensor_desc.plane_num  = gen_random_data(tensor_desc.plane_num);
-      sub_tensor_desc.cube_num   = gen_random_data(tensor_desc.cube_num);
+    void genSubTensor(){
+      subTensorDesc.baseAddr  = tensorDesc.baseAddr;
+      subTensorDesc.unitNum   = genRandomData(tensorDesc.unitNum);
+      subTensorDesc.sliceNum  = genRandomData(tensorDesc.sliceNum);
+      subTensorDesc.planeNum  = genRandomData(tensorDesc.planeNum);
+      subTensorDesc.cubeNum   = genRandomData(tensorDesc.cubeNum);
 
-      sub_tensor_desc.unit_skip  = tensor_desc.unit_skip;
-      sub_tensor_desc.slice_skip = tensor_desc.slice_skip;
-      sub_tensor_desc.plane_skip = tensor_desc.plane_skip;
-      sub_tensor_desc.cube_skip  = tensor_desc.cube_skip;
+      subTensorDesc.unitSkip  = tensorDesc.unitSkip;
+      subTensorDesc.sliceSkip = tensorDesc.sliceSkip;
+      subTensorDesc.planeSkip = tensorDesc.planeSkip;
+      subTensorDesc.cubeSkip  = tensorDesc.cubeSkip;
 
       cout <<"tensor descriptor:" << endl;
-      stringTensor(tensor_desc);
+      stringTensor(tensorDesc);
 
       cout <<"sub tensor descriptor:" << endl;
-      stringTensor(sub_tensor_desc);
+      stringTensor(subTensorDesc);
     }
 
-    int get_traversal_addr_point_num(micrarch_tensor_descriptor my_tensor_desc){
-      return my_tensor_desc.unit_num * my_tensor_desc.slice_num * my_tensor_desc.plane_num * my_tensor_desc.cube_num;
+    int getTraversalAddrPointNum(micrarchTensorDescriptor myTensorDesc){
+      return myTensorDesc.unitNum * myTensorDesc.sliceNum * myTensorDesc.planeNum * myTensorDesc.cubeNum;
     }
 
-    int get_skip_addr(int base_addr, int skip){
-      int raw_addr = base_addr + skip;
-      int skip_addr = raw_addr & (~(-1<<22));
+    int getSkipAddr(int baseAddr, int skip){
+      int rawAddr = baseAddr + skip;
+      int skipAddr = rawAddr & (~(-1<<22));
 
-      return skip_addr;
+      return skipAddr;
     }
 
-    int *traversal_tensor_addr_point(micrarch_tensor_descriptor my_tensor_desc){
-      int *traversalAddr = new int[get_traversal_addr_point_num(my_tensor_desc)];
+    int *traversalTensorAddrPoint(micrarchTensorDescriptor myTensorDesc){
+      int *traversalAddr = new int[getTraversalAddrPointNum(myTensorDesc)];
 
-      stringTensor(my_tensor_desc);
+      stringTensor(myTensorDesc);
 
-      int cube_addr  = my_tensor_desc.base_addr;
-      int plane_addr = cube_addr;
-      int slice_addr = cube_addr;
-      int unit_addr  = cube_addr;
+      int cubeAddr  = myTensorDesc.baseAddr;
+      int planeAddr = cubeAddr;
+      int sliceAddr = cubeAddr;
+      int unitAddr  = cubeAddr;
 
-      for(int cubeCnt=0; cubeCnt<my_tensor_desc.cube_num;cubeCnt++){
+      for(int cubeCnt=0; cubeCnt<myTensorDesc.cubeNum;cubeCnt++){
         cout << "cubeCnt= " << cubeCnt << endl;
-        plane_addr = cube_addr;
-        cube_addr = get_skip_addr(cube_addr, my_tensor_desc.cube_skip);
+        planeAddr = cubeAddr;
+        cubeAddr = getSkipAddr(cubeAddr, myTensorDesc.cubeSkip);
 
-        for(int planeCnt=0; planeCnt<my_tensor_desc.plane_num;planeCnt++){
+        for(int planeCnt=0; planeCnt<myTensorDesc.planeNum;planeCnt++){
           cout << "planeCnt = " << planeCnt << endl;
-          slice_addr = plane_addr;
-          plane_addr = get_skip_addr(plane_addr, my_tensor_desc.plane_skip);
+          sliceAddr = planeAddr;
+          planeAddr = getSkipAddr(planeAddr, myTensorDesc.planeSkip);
 
-          for(int sliceCnt=0; sliceCnt<my_tensor_desc.slice_num;sliceCnt++){
+          for(int sliceCnt=0; sliceCnt<myTensorDesc.sliceNum;sliceCnt++){
             cout << "sliceCnt = " << sliceCnt << endl;
-            unit_addr  = slice_addr;
-            slice_addr = get_skip_addr(slice_addr, my_tensor_desc.slice_skip);
+            unitAddr  = sliceAddr;
+            sliceAddr = getSkipAddr(sliceAddr, myTensorDesc.sliceSkip);
 
-            for(int unitCnt=0; unitCnt<my_tensor_desc.unit_num;unitCnt++){
+            for(int unitCnt=0; unitCnt<myTensorDesc.unitNum;unitCnt++){
               cout << "unitCnt = " << unitCnt << endl;
-              int idx = cubeCnt*my_tensor_desc.plane_num*my_tensor_desc.slice_num*my_tensor_desc.unit_num + planeCnt*my_tensor_desc.slice_num*my_tensor_desc.unit_num + sliceCnt*my_tensor_desc.unit_num + unitCnt;
+              int idx = cubeCnt*myTensorDesc.planeNum*myTensorDesc.sliceNum*myTensorDesc.unitNum + planeCnt*myTensorDesc.sliceNum*myTensorDesc.unitNum + sliceCnt*myTensorDesc.unitNum + unitCnt;
 
-              cout << "cubeCnt= " << cubeCnt << "planeCnt = " << planeCnt << "sliceCnt = " << sliceCnt << "unitCnt = " << unitCnt << endl;
-              cout << "idx = " << idx << ", unit_addr = " << dec << unit_addr << endl;
-              traversalAddr[idx] = unit_addr;
-              unit_addr += my_tensor_desc.unit_skip;
+              cout << "cubeCnt= " << cubeCnt << ", planeCnt = " << planeCnt << ", sliceCnt = " << sliceCnt << ", unitCnt = " << unitCnt << endl;
+              cout << "idx = " << idx << ", unitAddr = " << dec << unitAddr << endl;
+              traversalAddr[idx] = unitAddr;
+              unitAddr += myTensorDesc.unitSkip;
             }
             cout << "***** slice done ******" << endl;
             cout << "" << endl;
@@ -139,12 +139,12 @@ class tensor {
 int main(){
 
   tensor myTensor;
-  int *traversalAddr = myTensor.traversal_tensor_addr_point(myTensor.tensor_desc);
-  int tensor_addr_point_num = myTensor.get_traversal_addr_point_num(myTensor.tensor_desc);
-  myTensor.gen_sub_tensor();
-  int sub_tensor_addr_point_num = myTensor.get_traversal_addr_point_num(myTensor.sub_tensor_desc);
+  int *traversalAddr = myTensor.traversalTensorAddrPoint(myTensor.tensorDesc);
+  int tensorAddrPointNum = myTensor.getTraversalAddrPointNum(myTensor.tensorDesc);
+  myTensor.genSubTensor();
+  int subTensorAddrPointNum = myTensor.getTraversalAddrPointNum(myTensor.subTensorDesc);
 
-  // for(int i=0;i<addr_point_num;i++){
+  // for(int i=0;i<addrPointNum;i++){
   //   cout << "traversalAddr = " << traversalAddr[i] << endl;
   // }
 
