99bfa8ab92bb08ef8031789762633950f0d64efa
korben.dong <ghostyuan@outlook.com>
2025-01-06 23:23:14 +0800

    update tool

--- src/microarch_tool.cc
+++ src/microarch_tool.cc
@@ -11,19 +11,6 @@
 
 #include "microarch_tensor.hh"
 
-struct microarchTensorDescriptor{
-  int baseAddr;
-  int byteNum;
-  int unitNum;
-  int sliceNum;
-  int planeNum;
-  int cubeNum;
-  int unitSkip;
-  int sliceSkip;
-  int planeSkip;
-  int cubeSkip;
-};
-
 nlohmann::json readJSONFile(std::string fileName){
   std::ifstream jsonFile(fileName);
   nlohmann::json result;
@@ -43,26 +30,17 @@ nlohmann::json readJSONFile(std::string fileName){
 }
 
 int main(){
-  microarchTensorDescriptor tensorDesc;
 
   nlohmann::json configJSON = readJSONFile("../configs/demo.json");
 
-  tensorDesc.baseAddr  = configJSON["baseAddr"];
-  tensorDesc.byteNum   = configJSON["byteNum"];
-  tensorDesc.unitNum   = configJSON["unitNum"];
-  tensorDesc.sliceNum  = configJSON["sliceNum"];
-  tensorDesc.planeNum  = configJSON["planeNum"];
-  tensorDesc.unitSkip  = configJSON["unitSkip"];
-  tensorDesc.sliceSkip = configJSON["sliceSkip"];
-  tensorDesc.planeSkip = configJSON["planeSkip"];
-  tensorDesc.cubeSkip  = configJSON["cubeSkip"];
+  for (const auto& item: configJSON) {
+    microarchTensor myTensor(item["baseAddr"], item["byteNum"], item["unitNum"], item["sliceNum"], item["planeNum"], item["cubeNum"], item["unitSkip"], item["sliceSkip"], item["planeSkip"], item["cubeSkip"]);
 
-  microarchTensor myTensor(tensorDesc.baseAddr, tensorDesc.byteNum, tensorDesc.unitNum, tensorDesc.sliceNum, tensorDesc.planeNum, tensorDesc.cubeNum, tensorDesc.unitSkip, tensorDesc.sliceSkip, tensorDesc.planeSkip, tensorDesc.cubeSkip);
+    int *traversalAddr = myTensor.traversalTensorAddrPoint(myTensor.tensorDesc);
 
-  int *traversalAddr = myTensor.traversalTensorAddrPoint(myTensor.tensorDesc);
-
-  int tensorAddrPointNum = myTensor.getTraversalAddrPointNum(myTensor.tensorDesc);
-  cout <<"tensorAddrPointNum : " << tensorAddrPointNum << endl;
+    int tensorAddrPointNum = myTensor.getTraversalAddrPointNum(myTensor.tensorDesc);
+    cout <<"tensorAddrPointNum : " << tensorAddrPointNum << endl;
+  }
 
   return 0;
 }
